1690054cd1b02eb474c8b303ef050aa955a25083
TuLiZen <r56728916@yahoo.com.tw>
2018-03-22 14:32:17 +0800

    add !isset

--- a1.php
+++ a1.php
@@ -1,9 +1,13 @@
-<?php
-$a=$_GET["m"];
-$b=$_GET["n"];
-
-if($a>$b){
-echo $a-$b;
-}else{
-echo $b-$a;
-}
+﻿<?php
+if (!isset($_GET['aa'])
+	|| !isset($_GET['bb'])
+	|| !is_numeric($_GET['aa'])
+	|| !is_numeric($_GET['bb']) ){
+		echo"參數不正確";
+		exit;
+	}
+	$x=$_GET['aa'];
+	$y=$_GET['bb'];
+	$z=$x+$y;
+	
+	echo $z;
