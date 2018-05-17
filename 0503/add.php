5fae6ce85d3ab5a5a869bc732a908c07bd996ddd
TuLiZen <36947062+TuLiZen@users.noreply.github.com>
2018-05-17 16:56:42 +0800

    Add files via upload

--- 0503/add.php
+++ 0503/add.php
@@ -1,14 +1,21 @@
 <?php
-error_reporting(E_ALL ^ E_DEPRECATED);
+//error_reporting(0);
+//修改 php\ php.ini 中702行
+//
 //預期收到的資料格式
 //{prod:"商品名稱",price:"值"}
 
+//輸入=============================================================================
+//預期收到的資料格式 (prod"商品名稱" price"值")
 $data = jason_decode(file_get_contents('php://input'));
 
 //一開始測試用
 //echo $data->{"prod"} ;
 //echo $data->{"price"};
 
+//檢查你的資料，暫時省略
+
+//資料庫操庫==================================================================
 try {
 	$db = new PDO('mysql:host=localhost;dbname=test0329;charset=utf8'
 		,'mememe','123456', array( 
@@ -16,13 +23,18 @@ try {
 			PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
 		) );
 }catch(PDOException $err) {
-	echo "ERROR:";
-	echo $err->getMessage();  //真實世界不這樣做
-	echo '<a href="list.php">回到列表</a>';
+	http_response_code(500);
+	echo "failed";
+	echo $err->getMessage();  //測試時候使用
 	exit;
 }
 
 $stmt = $db->prepare('insert into moneybook (name,cost) values (?,?)');
 $stmt->execute([$_POST['prod'],$_POST['price']]);
 
-echo "ok";
+//輸出======================================================================
+$data->("id") = &db->lastInsertId(); //取得前一次insert 的id
+
+http_response_code(201);
+header("Content-Type","application/json;charset=UTF-8");
+echo json_encode($data); //把insert的資料再傳回給用戶端
